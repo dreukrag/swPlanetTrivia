aedd3c2ba6462cde5fad95b4d377852f6ec02282
Filipe <filipegroh@gmail.com>
2017-08-16 14:37:48 -0300

    Header component

--- src/components/HeaderComponent.js
+++ src/components/HeaderComponent.js
@@ -1,29 +1,41 @@
 import React from 'react'
 
 export default class HeaderComponent extends React.Component {
+
+    constructor(props) {
+        super(props)
+        this.state = {
+            hits: 0,
+            misses: 0
+        }
+    }
+
+    addHit = () =>{
+        this.setState({
+            hits:this.state.hits + 1
+        })
+    }
+
+    addMiss = () =>{
+        this.setState({
+            misses:this.state.misses+1
+        })
+    }
+
     render = () =>
         (
             <nav className="navbar navbar-inverse navbar-fixed-top">
                 <div className="container ">
-                    <ul className="nav navbar-nav ">
-                        <span className="brand navbar-link">
-                            <p className="navbar-text__top">Filipe</p>
-                            <p className="navbar-text__underline"></p>
-                            <p className="navbar-text__bottom">Alexandre Groh</p>
-                        </span>
+                    <ul className="nav navbar-nav navbar-left">
+                        <li>
+                            <span>Star Wars Planetary Trivia</span>
+                        </li>
                     </ul>
                     <ul className="nav navbar-nav navbar-right">
                         <li>
-                            <a href="#"><b>Home</b></a>
-                        </li>
-                        <li>
-                            <a href="#"><b>Gallery</b></a>
-                        </li>
-                        <li>
-                            <a href="#"><b>About</b></a>
-                        </li>
-                        <li>
-                            <a href="#"><b>Contact</b></a>
+                            <span>{this.hits}</span>
+                            <span>/</span>
+                            <span>{this.misses}</span>
                         </li>
                     </ul>
                 </div>
