bfaf05e0f4c581c96931c6352ae1a35dafd76efe
Filipe <filipegroh@gmail.com>
2017-08-16 14:42:57 -0300

    footer basico

--- src/components/FooterComponent.js
+++ src/components/FooterComponent.js
@@ -1,9 +1,19 @@
 import React from 'react'
 
-export class FooterComponent extends React.Component{
-    render = () =>(
-        <div>
-            
-        </div>
-    )
+export class FooterComponent extends React.Component {
+    render = () =>
+        (
+            <nav className="navbar navbar-inverse navbar-fixed-bottom">
+                <div className="container ">
+                    <ul className="nav navbar-nav navbar-left">
+
+                    </ul>
+                    <ul className="nav navbar-nav navbar-right">
+                        <li className="navbar-text">
+
+                        </li>
+                    </ul>
+                </div>
+            </nav>
+        )
 }
