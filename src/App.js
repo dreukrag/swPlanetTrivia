e6165502d056846f76b7e31d7b9dd3aa98e50ec2
Filipe <filipegroh@gmail.com>
2017-08-17 20:32:44 -0300

    nao repitir planetas que já apareceram

--- src/App.js
+++ src/App.js
@@ -2,15 +2,70 @@ import React, { Component } from 'react';
 import logo from './logo.svg';
 import './App.css';
 import { HeaderComponent, ContentComponent, FooterComponent } from './components'
+import { getPlanetsFromFilm } from './utils/api';
+
 
 class App extends Component {
   constructor(props) {
     super(props)
     this.state = {
       hits: 0,
-      misses: 0
+      misses: 0,
+      filmsList: [],
+      selPlanet: {
+        films: []
+      },
+      planetHistory: []
     }
   }
+
+  componentDidMount = () => {
+    getPlanetsFromFilm().then((rsp) => {
+      this.setState({
+        filmsList: rsp
+      });
+      this.pickRandomPlanet();
+    });
+  }
+
+  pickRandomPlanet = () => {
+    var n = Math.floor(this.state.filmsList.length * Math.random());
+
+    if (this.state.planetHistory.length == this.state.filmsList.length){
+      console.log('list has been fully played')
+      return;
+    }
+    if (this.state.planetHistory.indexOf(n) > -1) {
+      console.log('this planet has been picked already!')
+      this.pickRandomPlanet();
+      return;
+    }
+
+    this.setState({
+      selPlanet: this.state.filmsList[n]
+    });
+
+    this.blacklistPlanet(n);
+  }
+
+  planetPicker = (n) =>{
+    if(this.state.planetHistory.indexOf(n)>-1){
+      return false;
+    }
+  }
+
+  blacklistPlanet = (n) => {
+    console.log('blacklisting planet: ', n)
+    if (this.state.planetHistory.indexOf(n) > -1) {
+      console.log('planet is already black listed!')
+      return
+    }
+    this.setState({
+      planetHistory: this.state.planetHistory.concat(n)
+    })
+    console.log(this.state.planetHistory);
+  }
+
   render() {
     return (
       <div className="App">
@@ -24,7 +79,7 @@ class App extends Component {
         }}>
         </style>
         <HeaderComponent hits={this.state.hits} misses={this.state.misses} />
-        <ContentComponent hit_func={this.addHit} miss_func={this.addMiss} />
+        <ContentComponent hit_func={this.addHit} miss_func={this.addMiss} selPlanet={this.state.selPlanet} />
         <FooterComponent />
       </div>
     );
@@ -33,12 +88,14 @@ class App extends Component {
     this.setState({
       hits: this.state.hits + 1
     })
+    this.pickRandomPlanet();
   }
 
   addMiss = () => {
     this.setState({
       misses: this.state.misses + 1
     })
+    this.pickRandomPlanet();
   }
 }
 
