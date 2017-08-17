5b0aba74a640c055d2ca5a95a30dbb9443981e58
Filipe <filipegroh@gmail.com>
2017-08-17 19:16:52 -0300

    busca de planetas bruta filtrada por filme por id

--- src/utils/api.js
+++ src/utils/api.js
@@ -1,39 +1,17 @@
-export function getPlanets() {
-    return fetch('https://swapi.co/api/planets/')
-        .then((response) => response.json())
-        .then((responseJson) => {
-            return responseJson.count;
-        })
-        .catch((error) => {
-            console.error(error);
-        });
-}
-
-export function getPlanet(n) {
-    return fetch('https://swapi.co/api/planets/' + n)
-        .then((response) => response.json())
-        .then((responseJson) => {
-            return responseJson;
-        })
-        .catch((error) => {
-            console.error(error);
-        });
-}
-
 export function getPlanetList() {
     var list = [];
     var count = 0;
     function getList(url) {
-       return fetch(url)
+        return fetch(url)
             .then((response) => response.json())
             .then((responseJson) => {
-                list.push(responseJson.results);
+                list = list.concat(responseJson.results);
                 count += 1;
                 if (count > 10) { throw new Error("too many callbacks") }
                 return responseJson;
-            }).then((responseJson)=>{
+            }).then((responseJson) => {
                 if (responseJson.next != null) {
-                   return getList(responseJson.next)
+                    return getList(responseJson.next)
                 }
                 return list;
             })
@@ -42,11 +20,22 @@ export function getPlanetList() {
             });
     }
     return getList('https://swapi.co/api/planets/');
-    list.forEach((v,k)=>{
-        console.log(v);
-    })
-    return list;
+}
 
+export function getPlanet(n){
+    return fetch('https://swapi.co/api/planets/' + n)
+}
+
+export function getPlanetsFromFilm() {
+    return getPlanetList().then((rsp) => {
+        var listFilm = [];
+        rsp.forEach(function (element) {
+            if (element.films.length > 0) {
+                listFilm.push(element);
+            }
+        }, this);
+        return listFilm;
+    });
 }
 
 
