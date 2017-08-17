fc7b005cb0d5dea2fde3c0280416641e35f7a182
Filipe <filipegroh@gmail.com>
2017-08-17 19:14:43 -0300

    header n precisa de state, le props dado a ele pelo pai

--- src/components/HeaderComponent.js
+++ src/components/HeaderComponent.js
@@ -2,26 +2,6 @@ import React from 'react'
 
 export class HeaderComponent extends React.Component {
 
-    constructor(props) {
-        super(props)
-        this.state = {
-            hits: 0,
-            misses: 0
-        }
-    }
-
-    addHit = () =>{
-        this.setState({
-            hits:this.state.hits + 1
-        })
-    }
-
-    addMiss = () =>{
-        this.setState({
-            misses:this.state.misses+1
-        })
-    }
-
     render = () =>
         (
             <nav className="navbar navbar-inverse navbar-fixed-top">
@@ -33,9 +13,9 @@ export class HeaderComponent extends React.Component {
                     </ul>
                     <ul className="nav navbar-nav navbar-right">
                         <li className="navbar-text">
-                            <span>{this.state.hits}</span>
+                            <span>{this.props.hits}</span>
                             <span>/</span>
-                            <span>{this.state.misses}</span>
+                            <span>{this.props.misses}</span>
                         </li>
                     </ul>
                 </div>
