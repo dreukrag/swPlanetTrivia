a0b41d7ee8b23a0ab01745a5d356278bf4c39fb6
Filipe <filipegroh@gmail.com>
2017-08-18 21:31:32 -0300

    fontes, controle de estado, ajustes

--- src/App.js
+++ src/App.js
@@ -1,6 +1,8 @@
 import React, { Component } from 'react';
-import logo from './logo.svg';
+import font1 from './fonts/Starjedi.ttf';
+import font2 from './fonts/Starjhol.ttf';
 import './App.css';
+import './css/main.css';
 import { HeaderComponent, ContentComponent, FooterComponent } from './components'
 import { getPlanetsFromFilm } from './utils/api';
 
@@ -13,7 +15,12 @@ class App extends Component {
       misses: 0,
       filmsList: [],
       selPlanet: {
-        films: []
+        population: 'default value',
+        climate:'default value',
+        terrain:'default value',
+        films:{
+            length:0
+        }
       },
       planetHistory: [],
       isPlaying:false,
@@ -28,7 +35,7 @@ class App extends Component {
       });
       this.pickRandomPlanet();
       this.setState({
-        isPlaying:true
+        loaded:true
       })
     });
   }
@@ -37,7 +44,7 @@ class App extends Component {
     var n = Math.floor(this.state.filmsList.length * Math.random());
     if (this.state.planetHistory.length == this.state.filmsList.length) {
       console.log('list has been fully played')
-      console.log(this.state.planetHistory);    
+      // console.log(this.state.planetHistory);    
       this.setState({ isPlaying:false }); return;
     }
     if (this.state.planetHistory.indexOf(n) > -1) {this.pickRandomPlanet();return;}
@@ -48,7 +55,7 @@ class App extends Component {
   blacklistPlanet = (n) => {
     if (this.state.planetHistory.indexOf(n) > -1) { console.log('planet is already black listed!'); return; }
     this.setState({ planetHistory: this.state.planetHistory.concat(n) })
-    console.log(this.state.planetHistory);
+    // console.log(this.state.planetHistory);
   }
 
   render() {
@@ -56,12 +63,18 @@ class App extends Component {
       <div className="App">
         <style dangerouslySetInnerHTML={{
           __html: [
-            "@font-face{font-family:'star-wars'; src:url('src/fonts/Strjmono.ttf');}"
+            "@font-face{font-family:'star-wars1'; src:url('"+font1+"');}"
+          ]
+        }}>
+        </style>
+        <style dangerouslySetInnerHTML={{
+          __html: [
+            "@font-face{font-family:'star-wars2'; src:url('"+font2+"');}"
           ]
         }}>
         </style>
         <HeaderComponent hits={this.state.hits} misses={this.state.misses} />
-        <ContentComponent validateAnswer={this.validateAnswer} selPlanet={this.state.selPlanet} isPlaying={this.state.isPlaying}/>
+        <ContentComponent validateAnswer={this.validateAnswer} selPlanet={this.state.selPlanet} isPlaying={this.state.isPlaying} isLoading={this.state.loaded}/>
         <FooterComponent />
       </div>
     );
